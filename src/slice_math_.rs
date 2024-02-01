3036eca93d8449da3de93d867120706f9b85ac74
Sigurd Sætherø Spangelo <sigurd-s@online.no>
2024-02-02 00:05:18 +0100

    Add real FFT and IFFT

--- src/slice_math_.rs
+++ src/slice_math_.rs
@@ -1,7 +1,7 @@
 use std::{f64::consts::TAU, ops::{AddAssign, Mul, MulAssign}};
 
-use num::{complex::ComplexFloat, traits::{Inv, SaturatingSub}, Complex, Float, NumCast, Saturating, Zero};
-use slice_ops::SliceOps;
+use num::{complex::ComplexFloat, traits::{Inv, SaturatingSub}, Complex, Float, NumCast, One, Saturating, Zero};
+use slice_ops::{Slice, SliceOps};
 
 pub trait SliceMath<T>: SliceOps<T>
 {
@@ -17,28 +17,99 @@ pub trait SliceMath<T>: SliceOps<T>
         T: Mul<Rhs, Output: AddAssign + Zero> + Copy,
         Rhs: Copy,
         C: FromIterator<<T as Mul<Rhs>>::Output>;
-    fn convolve_fft_cooley_tukey<Rhs, C>(&self, rhs: &[Rhs]) -> C
+
+    /// Performs convolution using FFT.
+    /// 
+    /// # Examples
+    /// 
+    /// Convolution can be done directly `O(n^2)` or using FFT `O(nlog(n))`.
+    /// 
+    /// ```rust
+    /// use slice_math::*;
+    /// 
+    /// let x = [1.0, 0.0, 1.5, 0.0, 0.0, -1.0];
+    /// let h = [1.0, 0.6, 0.3];
+    /// 
+    /// let y_fft: Vec<f64> = x.convolve_fft(&h);
+    /// let y_direct: Vec<f64> = x.convolve_direct(&h);
+    /// 
+    /// let avg_error = y_fft.into_iter()
+    ///     .zip(y_direct.into_iter())
+    ///     .map(|(y_fft, y_direct)| (y_fft - y_direct).abs())
+    ///     .sum::<f64>()/x.len() as f64;
+    /// assert!(avg_error < 1.0e-15);
+    /// ```
+    fn convolve_fft<Rhs, C>(&self, rhs: &[Rhs]) -> C
     where
         T: Float + Copy,
         Rhs: Float + Copy,
-        Complex<T>: MulAssign + ComplexFloat<Real: Float> + From<Complex<<Complex<T> as ComplexFloat>::Real>> + Mul<Complex<Rhs>, Output: ComplexFloat<Real: Float>>,
-        Complex<Rhs>: MulAssign + ComplexFloat<Real: Float> + From<Complex<<Complex<Rhs> as ComplexFloat>::Real>>,
-        <Complex<T> as Mul<Complex<Rhs>>>::Output: MulAssign + ComplexFloat<Real: Float> + From<Complex<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>>,
+        Complex<T>: MulAssign + AddAssign + ComplexFloat<Real = T> + Mul<Complex<Rhs>, Output: ComplexFloat<Real: Float>>,
+        Complex<Rhs>: MulAssign + AddAssign + ComplexFloat<Real = Rhs>,
+        <Complex<T> as Mul<Complex<Rhs>>>::Output: ComplexFloat<Real: Float> + Into<Complex<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>>,
+        Complex<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>: MulAssign + AddAssign + ComplexFloat<Real = <<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>,
         C: FromIterator<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>;
     
-    fn fft_cooley_tukey(&mut self)
+    /// Performs an iterative, in-place radix-2 FFT algorithm as described in https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm#Data_reordering,_bit_reversal,_and_in-place_algorithms.
+    /// If length is not a power of two, it uses the DFT, which is a lot slower.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use num::Complex;
+    /// use slice_math::*;
+    /// 
+    /// let x = [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]
+    ///     .map(|x| <Complex<_> as From<_>>::from(x));
+    /// 
+    /// let mut y = x;
+    /// 
+    /// y.as_mut_slice().fft();
+    /// y.as_mut_slice().ifft();
+    /// 
+    /// let avg_error = x.into_iter()
+    ///     .zip(y.into_iter())
+    ///     .map(|(x, y)| (x - y).norm())
+    ///     .sum::<f64>()/x.len() as f64;
+    /// assert!(avg_error < 1.0e-16);
+    /// ```
+    fn fft(&mut self)
     where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<T::Real>>;
-    fn ifft_cooley_tukey(&mut self)
-    where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<T::Real>>;
+        T: ComplexFloat<Real: Float> + MulAssign + AddAssign + From<Complex<T::Real>>;
         
-    fn fft_rec_cooley_tukey(&mut self)
+    /// Performs an iterative, in-place radix-2 IFFT algorithm as described in https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm#Data_reordering,_bit_reversal,_and_in-place_algorithms.
+    /// If length is not a power of two, it uses the IDFT, which is a lot slower.
+    /// 
+    /// # Examples
+    /// ```rust
+    /// use num::Complex;
+    /// use slice_math::*;
+    /// 
+    /// let x = [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]
+    ///     .map(|x| <Complex<_> as From<_>>::from(x));
+    /// 
+    /// let mut y = x;
+    /// 
+    /// y.as_mut_slice().fft();
+    /// y.as_mut_slice().ifft();
+    /// 
+    /// let avg_error = x.into_iter()
+    ///     .zip(y.into_iter())
+    ///     .map(|(x, y)| (x - y).norm())
+    ///     .sum::<f64>()/x.len() as f64;
+    /// assert!(avg_error < 1.0e-16);
+    /// ```
+    fn ifft(&mut self)
     where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<T::Real>>;
-    fn ifft_rec_cooley_tukey(&mut self)
+        T: ComplexFloat<Real: Float> + MulAssign + AddAssign + From<Complex<T::Real>>;
+        
+    fn real_fft(&self, y: &mut [Complex<T>])
     where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<T::Real>>;
+        T: Float,
+        Complex<T>: ComplexFloat<Real = T> + MulAssign + AddAssign;
+        
+    fn real_ifft(&mut self, x: &[Complex<T>])
+    where
+        T: Float,
+        Complex<T>: ComplexFloat<Real = T> + MulAssign + AddAssign;
 }
 
 impl<T> SliceMath<T> for [T]
@@ -90,228 +161,223 @@ impl<T> SliceMath<T> for [T]
         }).collect()
     }
 
-    fn convolve_fft_cooley_tukey<Rhs, C>(&self, rhs: &[Rhs]) -> C
+    fn convolve_fft<Rhs, C>(&self, rhs: &[Rhs]) -> C
     where
         T: Float + Copy,
         Rhs: Float + Copy,
-        Complex<T>: MulAssign + ComplexFloat<Real: Float> + From<Complex<<Complex<T> as ComplexFloat>::Real>> + Mul<Complex<Rhs>, Output: ComplexFloat<Real: Float>>,
-        Complex<Rhs>: MulAssign + ComplexFloat<Real: Float> + From<Complex<<Complex<Rhs> as ComplexFloat>::Real>>,
-        <Complex<T> as Mul<Complex<Rhs>>>::Output: MulAssign + ComplexFloat<Real: Float> + From<Complex<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>>,
+        Complex<T>: MulAssign + AddAssign + ComplexFloat<Real = T> + Mul<Complex<Rhs>, Output: ComplexFloat<Real: Float>>,
+        Complex<Rhs>: MulAssign + AddAssign + ComplexFloat<Real = Rhs>,
+        <Complex<T> as Mul<Complex<Rhs>>>::Output: ComplexFloat<Real: Float> + Into<Complex<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>>,
+        Complex<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>: MulAssign + AddAssign + ComplexFloat<Real = <<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>,
         C: FromIterator<<<Complex<T> as Mul<Complex<Rhs>>>::Output as ComplexFloat>::Real>
     {
         let y_len = (self.len() + rhs.len()).saturating_sub(1);
         let len = y_len.next_power_of_two();
 
-        let mut x: Vec<Complex<T>> = self.into_iter()
-            .map(|&x| <Complex<T> as From<T>>::from(x))
-            .collect();
-        let mut h: Vec<Complex<Rhs>> = rhs.into_iter()
-            .map(|&h| <Complex<Rhs> as From<Rhs>>::from(h))
-            .collect();
+        let mut x: Vec<T> = self.to_vec();
+        let mut h: Vec<Rhs> = rhs.to_vec();
         
-        x.resize(len, Complex::zero());
-        h.resize(len, Complex::zero());
+        x.resize(len, T::zero());
+        h.resize(len, Rhs::zero());
 
-        x.fft_cooley_tukey();
-        h.fft_cooley_tukey();
+        let mut x_f = vec![Complex::zero(); len/2 + 1];
+        let mut h_f = vec![Complex::zero(); len/2 + 1];
 
-        let mut y: Vec<_> = x.into_iter().zip(h.into_iter()).map(|(x, h)| x*h).collect();
+        x.real_fft(&mut x_f);
+        h.real_fft(&mut h_f);
 
-        y.ifft_cooley_tukey();
+        let y_f: Vec<_> = x_f.into_iter()
+            .zip(h_f.into_iter())
+            .map(|(x_f, h_f)| (x_f*h_f).into())
+            .collect();
+
+        let mut y = vec![Zero::zero(); len];
+        y.real_ifft(&y_f);
 
         y.truncate(y_len);
         
         y.into_iter()
-            .map(|y| y.re())
             .collect()
     }
 
-    /// Performs an iterative, in-place radix-2 FFT algorithm as described in https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm#Data_reordering,_bit_reversal,_and_in-place_algorithms.
-    /// Length must be a power of two.
-    /// 
-    /// # Examples
-    /// ```rust
-    /// use num::Complex;
-    /// use slice_math::*;
-    /// 
-    /// let x = [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]
-    ///     .map(|x| <Complex<_> as From<_>>::from(x));
-    /// 
-    /// let mut y = x;
-    /// 
-    /// y.as_mut_slice().fft_cooley_tukey();
-    /// y.as_mut_slice().ifft_cooley_tukey();
-    /// 
-    /// let avg_error = x.into_iter()
-    ///     .zip(y.into_iter())
-    ///     .map(|(x, y)| (x - y).norm())
-    ///     .sum::<f64>()/x.len() as f64;
-    /// assert!(avg_error < 1.0e-16);
-    /// ```
-    fn fft_cooley_tukey(&mut self)
+    fn fft(&mut self)
     where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<<T>::Real>>
+        T: ComplexFloat<Real: Float> + MulAssign + AddAssign + From<Complex<<T>::Real>>
     {
         let len = self.len();
-        assert!(len.is_power_of_two(), "Length must be a power of two.");
-
-        self.bit_reverse_permutation();
         
-        for s in 0..len.ilog2()
+        if len.is_power_of_two()
         {
-            let m = 2usize << s;
-            let wm = <T as From<_>>::from(Complex::cis(<T::Real as NumCast>::from(-TAU/m as f64).unwrap()));
-            for k in (0..len).step_by(m)
+            // Radix 2 FFT
+
+            self.bit_reverse_permutation();
+            
+            for s in 0..len.ilog2()
             {
-                let mut w = T::one();
-                for j in 0..m/2
+                let m = 2usize << s;
+                let wm = <T as From<_>>::from(Complex::cis(<T::Real as NumCast>::from(-TAU/m as f64).unwrap()));
+                for k in (0..len).step_by(m)
                 {
-                    let t = w*self[k + j + m/2];
-                    let u = self[k + j];
-                    self[k + j] = u + t;
-                    self[k + j + m/2] = u - t;
-                    w *= wm;
+                    let mut w = T::one();
+                    for j in 0..m/2
+                    {
+                        let t = w*self[k + j + m/2];
+                        let u = self[k + j];
+                        self[k + j] = u + t;
+                        self[k + j + m/2] = u - t;
+                        w *= wm;
+                    }
                 }
+            }
+        }
+        else
+        {
+            // DFT
+
+            let wn = <T as From<_>>::from(Complex::cis(<T::Real as NumCast>::from(-TAU/len as f64).unwrap()));
+            let mut wnk = T::one();
+
+            let mut buf = vec![T::zero(); len];
+            unsafe {
+                std::ptr::swap_nonoverlapping(buf.as_mut_ptr(), self.as_mut_ptr(), len);
+            }
+            for k in 0..len
+            {
+                let mut wnki = T::one();
+                for i in 0..len
+                {
+                    self[k] += buf[i]*wnki;
+                    wnki *= wnk;
+                }
+
+                wnk *= wn;
             }
         }
     }
 
-    /// Performs an iterative, in-place radix-2 IFFT algorithm as described in https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm#Data_reordering,_bit_reversal,_and_in-place_algorithms.
-    /// Length must be a power of two.
-    /// 
-    /// # Examples
-    /// ```rust
-    /// use num::Complex;
-    /// use slice_math::*;
-    /// 
-    /// let x = [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]
-    ///     .map(|x| <Complex<_> as From<_>>::from(x));
-    /// 
-    /// let mut y = x;
-    /// 
-    /// y.as_mut_slice().fft_cooley_tukey();
-    /// y.as_mut_slice().ifft_cooley_tukey();
-    /// 
-    /// let avg_error = x.into_iter()
-    ///     .zip(y.into_iter())
-    ///     .map(|(x, y)| (x - y).norm())
-    ///     .sum::<f64>()/x.len() as f64;
-    /// assert!(avg_error < 1.0e-16);
-    /// ```
-    fn ifft_cooley_tukey(&mut self)
+    fn ifft(&mut self)
     where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<<T>::Real>>
+        T: ComplexFloat<Real: Float> + MulAssign + AddAssign + From<Complex<<T>::Real>>
     {
         let len = self.len();
-        assert!(len.is_power_of_two(), "Length must be a power of two.");
 
-        self.bit_reverse_permutation();
-        
-        for s in 0..len.ilog2()
+        if len.is_power_of_two()
         {
-            let m = 2usize << s;
-            let wm = <T as From<_>>::from(Complex::cis(<T::Real as NumCast>::from(TAU/m as f64).unwrap()));
-            for k in (0..len).step_by(m)
+            // Radix 2 IFFT
+
+            self.bit_reverse_permutation();
+            
+            for s in 0..len.ilog2()
             {
-                let mut w = T::one();
-                for j in 0..m/2
+                let m = 2usize << s;
+                let wm = <T as From<_>>::from(Complex::cis(<T::Real as NumCast>::from(TAU/m as f64).unwrap()));
+                for k in (0..len).step_by(m)
                 {
-                    let t = w*self[k + j + m/2];
-                    let u = self[k + j];
-                    self[k + j] = u + t;
-                    self[k + j + m/2] = u - t;
-                    w *= wm;
+                    let mut w = T::one();
+                    for j in 0..m/2
+                    {
+                        let t = w*self[k + j + m/2];
+                        let u = self[k + j];
+                        self[k + j] = u + t;
+                        self[k + j + m/2] = u - t;
+                        w *= wm;
+                    }
                 }
+            }
+        }
+        else
+        {
+            // IDFT
+
+            let wn = <T as From<_>>::from(Complex::cis(<T::Real as NumCast>::from(TAU/len as f64).unwrap()));
+            let mut wnk = T::one();
+
+            let mut buf = vec![T::zero(); len];
+            unsafe {
+                std::ptr::swap_nonoverlapping(buf.as_mut_ptr(), self.as_mut_ptr(), len);
+            }
+            for k in 0..len
+            {
+                let mut wnki = T::one();
+                for i in 0..len
+                {
+                    self[k] += buf[i]*wnki;
+                    wnki *= wnk;
+                }
+
+                wnk *= wn;
             }
         }
 
         self.mul_assign_all(<T as From<_>>::from(<Complex<_> as From<_>>::from(<T::Real as NumCast>::from(1.0/len as f64).unwrap())));
     }
     
-    /// Performs a recursive radix-2 FFT algorithm.
-    /// Length must be a power of two.
-    /// 
-    /// # Examples
-    /// ```rust
-    /// use num::Complex;
-    /// use slice_math::*;
-    /// 
-    /// let x = [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]
-    ///     .map(|x| <Complex<_> as From<_>>::from(x));
-    /// 
-    /// let mut y = x;
-    /// 
-    /// y.as_mut_slice().fft_rec_cooley_tukey();
-    /// y.as_mut_slice().ifft_rec_cooley_tukey();
-    /// 
-    /// assert_eq!(x, y);
-    /// ```
-    fn fft_rec_cooley_tukey(&mut self)
+    fn real_fft(&self, y: &mut [Complex<T>])
     where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<<T>::Real>>
+        T: Float,
+        Complex<T>: ComplexFloat<Real = T> + MulAssign + AddAssign
     {
         let len = self.len();
-        assert!(len.is_power_of_two(), "Length must be a power of two.");
 
         if len <= 1
         {
             return;
         }
 
-        let [even, odd] = self.spread_ref();
-        let mut even: Box<[T]> = even.into_iter()
-            .map(|x| **x)
-            .collect();
-        let mut odd: Box<[T]> = odd.into_iter()
-            .map(|x| **x)
+        assert_eq!(y.len(), len/2 + 1, "Invalid output buffer length.");
+
+        let mut x: Vec<Complex<T>> = self.into_iter()
+            .map(|&x| <Complex<_> as From<_>>::from(x))
             .collect();
 
-        even.fft_rec_cooley_tukey();
-        odd.fft_rec_cooley_tukey();
-
-        let wn = <T as From<_>>::from(Complex::cis(<T::Real as NumCast>::from(-TAU/len as f64).unwrap()));
-        let mut w = T::one();
-        for k in 0..len/2
+        x.fft();
+        
+        for (x, y) in x.into_iter()
+            .zip(y.iter_mut())
         {
-            let p = even[k];
-            let q = w*odd[k];
-            self[k] = p + q;
-            self[k + len/2] = p - q;
-            w *= wn;
+            *y = x;
         }
     }
-
-    /// Performs a recursive radix-2 IFFT algorithm.
-    /// Length must be a power of two.
-    /// 
-    /// # Examples
-    /// ```rust
-    /// use num::Complex;
-    /// use slice_math::*;
-    /// 
-    /// let x = [1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]
-    ///     .map(|x| <Complex<_> as From<_>>::from(x));
-    /// 
-    /// let mut y = x;
-    /// 
-    /// y.as_mut_slice().fft_rec_cooley_tukey();
-    /// y.as_mut_slice().ifft_rec_cooley_tukey();
-    /// 
-    /// assert_eq!(x, y);
-    /// ```
-    fn ifft_rec_cooley_tukey(&mut self)
+        
+    fn real_ifft(&mut self, x: &[Complex<T>])
     where
-        T: ComplexFloat<Real: Float> + MulAssign + From<Complex<<T>::Real>>
+        T: Float,
+        Complex<T>: ComplexFloat<Real = T> + MulAssign + AddAssign
     {
         let len = self.len();
-        assert!(len.is_power_of_two(), "Length must be a power of two.");
 
-        self.conj_assign_all();
+        if len <= 1
+        {
+            return;
+        }
 
-        self.fft_rec_cooley_tukey();
+        assert_eq!(x.len(), len/2 + 1, "Invalid input buffer length.");
 
-        self.conj_assign_all();
+        let mut x: Vec<Complex<T>> = x.into_iter()
+            .map(|&x| x)
+            .chain(x[1..(x.len() + len % 2 - 1)].into_iter()
+                .rev()
+                .map(|&x| x.conj())
+            ).collect();
         
-        self.mul_assign_all(<T as From<_>>::from(<Complex<_> as From<_>>::from(<T::Real as NumCast>::from(1.0/len as f64).unwrap())));
+        x.ifft();
+
+        for (x, y) in x.into_iter()
+            .zip(self.iter_mut())
+        {
+            *y = x.re();
+        }
     }
+}
+
+#[test]
+fn test()
+{
+    let mut x = [1.0, 1.0, 0.0, 0.0];
+    let mut y = [Complex::zero(); 3];
+
+    x.real_fft(&mut y);
+    x.real_ifft(&y);
+
+    println!("{:?}", x)
 }
